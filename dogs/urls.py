4eb519956a851b66191a9fd098a137c59913974e
sohakes <sohakes@gmail.com>
2013-10-13 21:36:09 -0300

    mais uns views

--- dogs/urls.py
+++ dogs/urls.py
@@ -6,6 +6,7 @@ from dogs import persons_views
 urlpatterns = patterns('',
    url(r'^$', dogs_views.index, name='index'),
    url(r'^persons/create$', persons_views.create, name='create-person'),
+   url(r'^persons/edit/$', persons_views.create, name='edit-person'),
    url(r'^dogs/create$', dogs_views.create, name='create-dog'),
    url(r'^dogs/(?P<dog_id>\d+)/$', dogs_views.detail, name='detail-dog'),
    url(r'^persons/(?P<person_username>\w+)/$', persons_views.detail, name='detail-person'),
